1c97cb027d5035c906711cceaaa584401f9faa0b
Leslie Counterman <LCounterman1@gmail.com>
2022-01-05 20:30:22 -0500

    second challenge

--- CodeChallengeRedBadge_Console/Program.cs
+++ CodeChallengeRedBadge_Console/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CodeChallengeRedBadge_Console
@@ -10,6 +11,37 @@ namespace CodeChallengeRedBadge_Console
     {
         static void Main(string[] args)
         {
+            //Challenge 1
+            
+            String[] strings = new string[4] {"abc", "abc", "df", "kjl" };
+
+            strings.Distinct().ToList().ForEach(s => Console.WriteLine(s));
+            Console.ReadKey();
+            
+
+
+
+            //Challenge 2
+
+            Regex rx = new Regex(@"\b(?<word>\w+)\s+(\k<word>\b");
+
+            string text = "there once was a tree in a valley";
+
+            MatchCollection matches = rx.Matches(text);
+
+            Console.WriteLine("{0} matches found in:\n   {1}",
+                matches.Count,
+                text);
+
+            foreach (Match match in matches)
+            {
+                GroupCollection groups = match.Groups;
+                Console.WriteLine("'{0}' repeated at positions {1} and {2}",
+                    groups["word"].Value,
+                    groups[0].Index,
+                    groups[1].Index);
+            }
+
         }
     }
 }
